a13dda1650fce039535ce52e508e69974e2685f8
Sahilghoghari07 <work.sahilghoghari@gmail.com>
2026-03-25 22:02:21 +0530

    Global error handler middleware added

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/users", userRoutes);
 
-app.use(errorHandler);
+app.use(errorHandler); //global middlewares at last
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
